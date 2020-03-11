44ec08d04893f4cdd1f2373489a486860b5b6720
Ismael Balafrej <ismael.balafrej@usherbrooke.ca>
2020-03-10 21:17:39 -0400

    Added solution to challenge 2

---
+++ challenge_2/solve.py
@@ -0,0 +1,33 @@
+from itertools import permutations
+import numpy as np
+from tqdm import tqdm
+
+# Sieve of eratosthenes
+N = int(1e6)
+l = np.ones(N, dtype=bool)
+for i in np.arange(2, N):
+    if l[i]:
+        l[i*i::i] = False
+
+count = 1
+circular_primes = [2]
+for i in tqdm(np.arange(3, N, 2)):
+    if l[i]:
+        strp = str(i)
+        if '2' in strp:
+            continue
+        possibilities = set(map(lambda i: int("".join(i)), permutations(strp)))
+        all_primes = True
+        for j in possibilities:
+            if l[j]:
+                l[j] = False
+            else:
+                all_primes = False
+                break
+        if all_primes:
+            count += len(possibilities)
+            circular_primes += possibilities
+
+print(circular_primes)           
+print(count)
+
